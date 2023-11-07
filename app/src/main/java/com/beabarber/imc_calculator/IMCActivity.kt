67e4cb957f94f920fec06fdfa0e5fd8be80b6050
Beatriz <bebarmar@alumni.uv.es>
2023-11-07 18:44:09 +0100

    Initial commit

---
+++ app/src/main/java/com/beabarber/imc_calculator/IMCActivity.kt
@@ -0,0 +1,12 @@
+package com.beabarber.imc_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IMCActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+    }
+}
---
+++ app/src/main/java/com/beabarber/imc_calculator/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.beabarber.imc_calculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.btn)
+        btn.setOnClickListener { navigateToIMC() }
+        val btnIMC = findViewById<Button>(R.id.btnIMC)
+        btn.setOnClickListener { navigateToIMC() }
+    }
+
+    private fun navigateToIMC(){
+        val intent = Intent(this, IMCActivity::class.java)
+        startActivity(intent)
+    }
+}
